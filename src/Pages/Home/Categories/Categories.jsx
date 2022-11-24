b7de45066c657ed71394ed71d5fd7fd6e8a1e5fe
Ador Zamaan <adorzamaan@gmail.com>
2022-11-24 21:49:20 +0600

    categories part

---
+++ src/Pages/Home/Categories/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div>
+      <div className="py-6 text-center">
+        <h3 className="font-bold pb-1">Let's Find your Dream Car</h3>
+        <p>
+          We recommended the very best and new cars toady and also friendly
+          Price for you
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
