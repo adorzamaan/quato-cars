4b738598edbfebf0a42d52a03c147f433d6d718a
Ador Zamaan <adorzamaan@gmail.com>
2022-11-25 01:45:38 +0600

    seller profile

--- src/Pages/Service/CateGoryCard/CategoryCard.jsx
+++ src/Pages/Service/CateGoryCard/CategoryCard.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { authContext } from "../../../Context/AuthProvider";
 import PrimaryButton from "../../../Shared/PrimaryButton/PrimaryButton";
 
 const CategoryCard = ({ service }) => {
+  const { user } = useContext(authContext);
   const { description } = service;
   console.log(description);
   return (
@@ -20,17 +22,24 @@ const CategoryCard = ({ service }) => {
             </p>
             <p style={{ fontSize: "16px" }}>CC : {service?.Cc}</p>
             <p style={{ fontSize: "16px" }}>Registration : {service?.Reg}</p>
-            <p style={{ fontSize: "16px" }}>Condition : {service?.condition[2]}</p>
-            <p style={{ fontSize: "16px" }}>Year of  Use : {service?.yearuse}</p>
-            <p style={{ fontSize: "16px" }}>ResellPrice : {service?.resellprice}</p>
-            <p style={{ fontSize: "16px" }}>MarketPrice : {service?.orginalPrice}</p>
+            <p style={{ fontSize: "16px" }}>
+              Condition : {service?.condition[2]}
+            </p>
+            <p style={{ fontSize: "16px" }}>Year of Use : {service?.yearuse}</p>
+            <p style={{ fontSize: "16px" }}>
+              ResellPrice : {service?.resellprice}
+            </p>
+            <p style={{ fontSize: "16px" }}>
+              MarketPrice : {service?.orginalPrice}
+            </p>
+            <p style={{ fontSize: "16px" }}>Location : {service?.location}</p>
 
             <div className="divider">
               <p className="font-medium text-sm text-gray-800 border-gray-300">
-            Detail Info
+                Detail Info
               </p>
             </div>
-            {description?.slice(0,3).map((des, index) => (
+            {description?.slice(0, 3).map((des, index) => (
               <p key={index} className="flex items-center px-2 py-1">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -47,6 +56,18 @@ const CategoryCard = ({ service }) => {
                 {des}
               </p>
             ))}
+
+            <div className="divider">
+              <p className="font-medium text-sm text-gray-800 border-gray-300">
+                Seller Info
+              </p>
+            </div>
+            <div className="avatar flex justify-center">
+              <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-1">
+                <img src={user?.photoURL} alt="/" />
+              </div>
+            </div>
+            <h3 className="font-bold text-md text-center pt-2">{user?.displayName}</h3>
           </div>
           {/* <div className="tablet">
             <p>
@@ -62,7 +83,7 @@ const CategoryCard = ({ service }) => {
             {/* <button className="px-8 py-1 bg-purple-600 text-white">
               Purchase Now
             </button> */}
-            <PrimaryButton>Visit Detail</PrimaryButton>
+            <PrimaryButton>Book Now</PrimaryButton>
           </Link>
         </div>
       </div>
